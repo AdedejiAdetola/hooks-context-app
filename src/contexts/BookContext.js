c3660ea22fa1ba2a61103ae82aba505dd54f38e5
unknown <adetolaadedeji0@gmail.com>
2022-07-04 18:07:02 +0100

    update app

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,14 +1,14 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext} from 'react';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
+    const books = [
         {title: 'name of the wind', id:1},
         {title: 'the way of kings', id:2},
         {title: 'the final empire', id:3},
         {title: 'the hero of ages', id:4}
-    ]);
+    ];
     return ( 
         <BookContext.Provider value={{books}}>
             {props.children}
